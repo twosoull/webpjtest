e3f112b19926329b944b06403e0eee0822583436
twosoull <71281491+twosoull@users.noreply.github.com>
2021-06-10 07:33:31 +0900

    tiles.xml 생성

--- src/main/java/com/javaex/controller/notice/ListController.java
+++ src/main/java/com/javaex/controller/notice/ListController.java
@@ -11,7 +11,7 @@ public class ListController implements Controller{
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		ModelAndView mv = new ModelAndView("notice/list");
+		ModelAndView mv = new ModelAndView("notice.list");
 		
 		
 		return mv;
